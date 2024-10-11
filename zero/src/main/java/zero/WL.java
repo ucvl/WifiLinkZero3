282286cfc996db159f9aa13679268ad0d4f2faa5
admin <admin@DESKTOP-BQ5KUFC.mshome.net>
2024-10-11 15:06:15 +0800

    serial

--- zero/src/main/java/zero/WL.java
+++ zero/src/main/java/zero/WL.java
@@ -1,20 +1,63 @@
 package zero;
 
+import java.io.IOException;
+
+import com.pi4j.io.serial.Baud;
+import com.pi4j.io.serial.DataBits;
+import com.pi4j.io.serial.FlowControl;
+import com.pi4j.io.serial.Parity;
+import com.pi4j.io.serial.Serial;
+import com.pi4j.io.serial.SerialFactory;
+import com.pi4j.io.serial.SerialPortException;
+import com.pi4j.io.serial.StopBits;
+
 import common.GetLastVersion;
 
 public class WL {
 
    
-    public static void main(String[] args)    {
-
+    public static void main(String[] args)     {
+    	   final Serial serial = SerialFactory.createInstance();
+    	   
         // 主程序逻辑
     	 System.out.println(    GetLastVersion.getLatestVersion());
         while (true) {
             // 模拟主程序运行
-            System.out.println("念天地之悠悠，独怆然而涕下！");
+            System.out.println("准备开串口！");
             try {
                 Thread.sleep(1000);
            
+                
+                try {
+                    // 打开串口
+   
+                    serial.open("/dev/ttyS5", Baud._19200, DataBits._8, Parity.NONE, StopBits._1, FlowControl.NONE);
+
+                    System.out.println("串口已打开");
+
+                    // 等待5秒钟
+                    Thread.sleep(5000);
+
+                } catch (SerialPortException | InterruptedException | IOException ex) {
+                    System.out.println("操作失败: " + ex.getMessage());
+                } finally {
+                    // 关闭串口
+                    if (serial.isOpen()) {
+                        try {
+                            serial.close();
+                            System.out.println("串口已关闭");
+                        } catch (IllegalStateException | IOException ex) {
+                            System.out.println("关闭串口失败: " + ex.getMessage());
+                        }
+                    }
+                }
+                
+                
+                
+                
+                
+                
+                
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
